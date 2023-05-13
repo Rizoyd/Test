99a63d03f934000dbc023bc60770ecb92746238a
Rizoyd <irizoyd@gmail.com>
2023-05-13 12:39:59 +0300

    MoviesEasy

--- index.js
+++ index.js
@@ -16,11 +16,43 @@
 // console.log(result);
 
 
-const lines = 25;
-let result = '';
-for(let star=1,count = lines;star<=lines;star++,count--){
-    result += ' '.repeat(count) 
-    result += '* '.repeat(star)
-    result += '\n'
+// const lines = 6;
+// let result = '';
+// for(let star=1,count = lines;star<=lines;star++,count--){
+//     result += ' '.repeat(count-1) 
+//     result += '*'.repeat(star*2-1)
+//     result += '\n'
+// }
+// console.log(result)
+
+// function fourthTask() {
+//     let i = 2
+//     while(i<=16){
+//         if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+//         i++
+//     }
+// }
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [...data].reverse();
+// console.log(result)
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','')
+personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    generes:[],
+    privat: false,
 }
-console.log(result)
+const a =prompt("Один из последних просмотренных филом?",''),
+      b =prompt("На сколько оцените его?",''),
+      c =prompt("Один из последних просмотренных филом?",''),
+      d =prompt("На сколько оцените его?",'');
+personalMovieDB.movies[a]=b
+personalMovieDB.movies[c]=d
+console.log(personalMovieDB)
